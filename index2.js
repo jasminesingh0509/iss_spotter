7ee62c798d8761d0ebb1161b73567dc994089baf
Jasmine <59420077+jasminesingh0509@users.noreply.github.com>
2020-02-15 19:24:02 -0800

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,10 @@
+const { fetchMyIP } = require('./iss_promised');
+const { fetchCoordsByIP } = require('./iss_promised');
+const { fetchISSFlyOverTime } = require('./iss_promised')
+
+
+
+fetchMyIP()
+.then(fetchCoordsByIP)
+.then(fetchISSFlyOverTimes)
+.then(body => console.log(body));
