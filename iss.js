79ad5079bbfcd962e652dbac8aa1f7fec1e9d486
Jasmine <59420077+jasminesingh0509@users.noreply.github.com>
2020-02-22 13:36:57 -0800

    Update iss.js
    added changes manually, commit not working

--- iss.js
+++ iss.js
@@ -47,6 +47,57 @@ const fetchCoordsByIP = function(ip, callback) {
 
 
 
+const fetchISSFlyOverTimes = function (coords, callback) {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}` , (error, response, body) => {
+
+  if (error) {
+    callback(error, null);
+    return;
+
+  }
+  if (response.statusCode !== 200) {
+    const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+    callback(Error(msg), null);
+    return;
+  }
+});
+};
+
+
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results. 
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */ 
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+  
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+  
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+  
+        callback(null, nextPasses);
+      });
+    });
+  });
+};
+
+
+
+
 
 
 // const request = require("request");
@@ -76,4 +127,6 @@ const fetchCoordsByIP = function(ip, callback) {
 
 
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
+
+
